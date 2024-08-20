d0af4c53eb686b41c23b4672a249f65e5c31b5c0
Yemezzy <ayubayelmee222@gmail.com>
2024-08-21 00:41:52 +0100

    first commit

--- src/server.ts
+++ src/server.ts
@@ -12,8 +12,8 @@ const PORT = process.env.PORT;
 
 app.use(
   cors({
-    origin: "http://www.investorgrain.com",
-    // origin: "http://localhost:3000",
+    // origin: "http://www.investorgrain.com",
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
@@ -21,8 +21,8 @@ app.use(
 app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "http://www.investorgrain.com"
-    // "http://localhost:3000"
+    // "http://www.investorgrain.com"
+    "http://localhost:3000"
   );
   res.header(
     "Access-Control-Allow-Methods",
